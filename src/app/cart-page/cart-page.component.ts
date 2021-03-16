178d9573a92132440752e7523674961df2e72f39
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-03-16 14:12:49 +0200

    show from localstorage in cart

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
-  styleUrls: ['./cart-page.component.css']
+  styleUrls: ['./cart-page.component.css'],
 })
 export class CartPageComponent implements OnInit {
+  cartProds = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.cartProds = this.allStorage();
+    console.log(this.cartProds);
   }
+  allStorage() {
+    var values = [],
+      keys = Object.keys(localStorage),
+      i = keys.length;
 
+    while (i--) {
+      values.push(JSON.parse(localStorage.getItem(keys[i])));
+    }
+
+    return values;
+  }
 }
