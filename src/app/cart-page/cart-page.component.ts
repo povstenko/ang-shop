5a2cbe8374e924ae000cf7107d6cd20d2873d39b
Vitaliy Povstenko <vitaly.povstenko@gmail.com>
2021-03-21 13:39:38 +0200

    Add Remove amount in cart

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -20,18 +20,29 @@ export class CartPageComponent implements OnInit {
   updateSum() {
     this.prod_sum = 0;
     this.cartProds.forEach((i) => {
-      this.prod_sum += i.price;
+      this.prod_sum += i.price * i.amount;
     });
   }
 
   delProd(product) {
-    this.cartProds = this.cartProds.filter((p) => {
-      return product.id != p.id;
-    });
-
-    localStorage.removeItem(product.id);
+    if (product.amount == 1) {
+      this.cartProds = this.cartProds.filter((p) => {
+        return product.id != p.id;
+      });
+      localStorage.removeItem(product.id);
+    } else {
+      product.amount--;
+      localStorage.setItem(product.id.toString(), JSON.stringify(product));
+    }
     this.updateSum();
   }
+
+  addProd(product) {
+    product.amount++;
+    localStorage.setItem(product.id.toString(), JSON.stringify(product));
+    this.updateSum();
+  }
+
   allStorage() {
     var values = [],
       keys = Object.keys(localStorage),
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -3,20 +3,25 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-
   @Input() product;
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   addProd(product) {
-    // console.log(product);
-    localStorage.setItem(product.id.toString(), JSON.stringify(product));
-    
-  }
+    let storage_prod = JSON.parse(localStorage.getItem(product.id));
 
+    if(storage_prod != null) {
+      console.log("product exists");
+      product["amount"] = storage_prod["amount"] + 1;
+    }
+    else {
+      console.log("new product");
+      product["amount"] = 1;
+    }
+    localStorage.setItem(product.id.toString(), JSON.stringify(product));
+  }
 }
